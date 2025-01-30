db3ac10f50d6cc4322b7bdb926be795ba1b258a1
Sarra_boudrigua <sarra.boudrigua@etud.univ-jfc.fr>
2025-01-30 15:14:50 +0100

    starting page component

---
+++ frontend/src/app/StartingPage/start-page/start-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-page',
+  imports: [],
+  templateUrl: './start-page.component.html',
+  standalone: true,
+  styleUrl: './start-page.component.css'
+})
+export class StartPageComponent {
+
+}
