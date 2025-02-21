5bea94609004229d336326ebb2748e5598e6efca
tis007 <31tis007@gmail.com>
2025-02-21 09:20:24 +0100

    added graphql requests

--- frontend/src/app/webservice.service.ts
+++ frontend/src/app/webservice.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Client, fetchExchange} from '@urql/core';
-import GET_WORLD from './Grapqhrequests';
+import {Product} from './schema';
+import {ACHETER_PRODUIT, ENGAGER_MANAGER, GET_WORLD, LANCER_PRODUCTION, ACHETER_ANGEL_UPGRADE, ACHETER_CASH_UPGRADE, RESET_WORLD} from './Grapqhrequests';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +12,60 @@ export class WebserviceService {
   user = 'Toto';
 
   createClient() {
-    return new Client({ url:this.server +'graphql',
-      exchanges: [fetchExchange] });
+    return new Client({
+      url: this.server + 'graphql',
+      exchanges: [fetchExchange]
+    });
   }
 
-  getWorld(user: string) {
-    return this.createClient().query(GET_WORLD, {"user":user}).toPromise().then(response => {
-      console.log('Response:', response);
-      return response;
-  })
+  async getWorld(user: string) {
+    const response = await this.createClient().query(GET_WORLD, {"user": user}).toPromise();
+    console.log('Response:', response);
+    return response;
   }
 
-  constructor() { }
+  async lancerProduction(user: string, product: Product) {
+    return await this.createClient().mutation(LANCER_PRODUCTION, {
+      user: user,
+      id:
+      product.id
+    }).toPromise();
+  }
+
+  async engagerManager(user: string, product: Product) {
+    return await this.createClient().mutation(ENGAGER_MANAGER, {
+      user: user,
+      name: product.name
+    }).toPromise();
+  }
+
+  async acheterQtProduit(user: string, product: Product, quantite: number) {
+    return await this.createClient().mutation(ACHETER_PRODUIT, {
+      user: user,
+      id: product.id,
+      quantite: quantite
+    }).toPromise();
+  }
+
+  async acheterCashUpgrade(user: string, id: number) {
+    return await this.createClient().mutation(ACHETER_CASH_UPGRADE, {
+      user,
+      id
+    }).toPromise();
+  }
+
+  async acheterAngelUpgrade(user: string, id: number) {
+    return await this.createClient().mutation(ACHETER_ANGEL_UPGRADE, {
+      user,
+      id
+    }).toPromise();
+  }
+
+  async resetWorld(user: string) {
+    return await this.createClient().mutation(RESET_WORLD, {
+      user
+    }).toPromise();
+  }
+  constructor() {
+  }
 }
