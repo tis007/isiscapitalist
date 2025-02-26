f2c4b30a84bccdfd94d51658d50fd18d56689fa5
Sarra_boudrigua <sarra.boudrigua@etud.univ-jfc.fr>
2025-02-26 20:36:53 +0100

    upgrades done!!

--- frontend/src/app/World/investors/investors-w1.component.ts
+++ frontend/src/app/World/investors/investors-w1.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {WebserviceService} from '../../webservice.service';
+import {World} from '../../schema';
 
 @Component({
   selector: 'app-investors-w1',
@@ -8,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './investors-w1.component.css'
 })
 export class InvestorsW1Component {
+  constructor(private service: WebserviceService) {
+    this.server = service.server
+  }
+  world: World =new World();
 
+  server: string;
+  @Input()
+  set wor(value: World) {
+    this.world = value;
+  }
 }
--- frontend/src/app/World/upgrades/upgrades-w1.component.ts
+++ frontend/src/app/World/upgrades/upgrades-w1.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {WebserviceService} from '../../webservice.service';
+import {World} from '../../schema';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-upgrades-w1',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    NgClass
+  ],
   templateUrl: './upgrades-w1.component.html',
   standalone: true,
   styleUrl: './upgrades-w1.component.css'
 })
 export class UpgradesW1Component {
+  constructor(private service: WebserviceService) {
+    this.server = service.server
+  }
+  world: World =new World();
+
+  server: string;
+  selectedCategory: string ='cash';
+  @Input()
+  set wor(value: World) {
+    this.world = value;
+  }
 
 }
